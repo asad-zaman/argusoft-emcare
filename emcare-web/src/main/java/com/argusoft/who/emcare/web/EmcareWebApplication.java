756b77b83317e11077a4b60ed892fc4a6c233e86
jkalariya <jkalariya@argusoft.in>
2021-11-24 20:58:51 +0530

    Implement Keycloak functionality

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/EmcareWebApplication.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/EmcareWebApplication.java
@@ -3,11 +3,18 @@ package com.argusoft.who.emcare.web;
 import com.argusoft.who.emcare.web.fhir.FhirServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class EmcareWebApplication {
+@EnableTransactionManagement
+@ComponentScan(basePackages = "com.argusoft.who.emcare.web")
+@EntityScan(basePackages = "com.argusoft.who.emcare.web")
+public class EmcareWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EmcareWebApplication.class, args);
