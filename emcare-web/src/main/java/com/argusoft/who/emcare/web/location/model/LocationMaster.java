e5f31d263d1003fa7f6819fcd90b9beebf742836
jkalariya <jkalariya@argusoft.in>
2021-11-29 18:25:21 +0530

    Add simple location manage api

---
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/location/model/LocationMaster.java
@@ -0,0 +1,56 @@
+package com.argusoft.who.emcare.web.location.model;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+/**
+ *
+ * @author jay
+ */
+@Data
+@Entity
+@Table(name = "location_master")
+public class LocationMaster implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Basic(optional = false)
+    @Column(name = "name", nullable = false, length = 4000)
+    private String name;
+
+    @Basic(optional = false)
+    @Column(name = "type", nullable = false, length = 10)
+    private String type;
+
+    @Basic(optional = false)
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+
+//        @Basic(optional = false)
+//        @Column(name = "created_by", nullable = false, length = 50)
+//        private String createdBy;
+//
+//        @Basic(optional = false)
+//        @Column(name = "created_on", nullable = false)
+//        @Temporal(TemporalType.TIMESTAMP)
+//        private Date createdOn;
+//
+//        @Column(name = "modified_by", length = 50)
+//        private String modifiedBy;
+//        @Column(name = "modified_on")
+//        @Temporal(TemporalType.TIMESTAMP)
+//        private Date modifiedOn;
+    @Column(name = "parent")
+    private Long parent;
+
+}
