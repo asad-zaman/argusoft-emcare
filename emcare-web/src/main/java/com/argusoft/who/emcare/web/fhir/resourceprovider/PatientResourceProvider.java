6b60113bb22e142b4fa600838b0ed355a0fd78e5
Parth Panchal <ppanchal@argusoft.in>
2021-11-29 14:54:17 +0530

    Added emcareResource DAO, model and service, patient create method in Resource Provider, and changed servlet bean structure.

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/fhir/resourceprovider/PatientResourceProvider.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/fhir/resourceprovider/PatientResourceProvider.java
@@ -80,7 +80,7 @@ public class PatientResourceProvider implements IResourceProvider {
      */
     @Search()
     public List<Patient> getPatient(@RequiredParam(name = Patient.SP_FAMILY) StringParam theFamilyName) {
-        Patient patient = new Patient();
+    	Patient patient = new Patient();
         patient.addIdentifier();
         patient.getIdentifier().get(0).setUse(Identifier.IdentifierUse.OFFICIAL);
         patient.getIdentifier().get(0).setSystem("urn:hapitest:mrns");
