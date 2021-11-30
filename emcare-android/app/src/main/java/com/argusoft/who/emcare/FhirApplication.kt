0f7d5ec6b4e5b8ac27a4ab5d836ebb52697b95fc
Kunjan <kunjanp@argusoft.com>
2021-11-30 15:51:53 +0530

    Initial Android Commit

---
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/FhirApplication.kt
@@ -0,0 +1,27 @@
+package com.argusoft.who.emcare
+
+import android.app.Application
+import android.content.Context
+import com.argusoft.who.emcare.data.FhirPeriodicSyncWorker
+import com.google.android.fhir.FhirEngine
+import com.google.android.fhir.FhirEngineProvider
+import com.google.android.fhir.sync.Sync
+
+class FhirApplication : Application() {
+  // Only initiate the FhirEngine when used for the first time, not when the app is created.
+  private val fhirEngine: FhirEngine by lazy { constructFhirEngine() }
+
+  override fun onCreate() {
+    super.onCreate()
+    Sync.oneTimeSync<FhirPeriodicSyncWorker>(this)
+  }
+
+
+  private fun constructFhirEngine(): FhirEngine {
+    return FhirEngineProvider.getInstance(this)
+  }
+
+  companion object {
+    fun fhirEngine(context: Context) = (context.applicationContext as FhirApplication).fhirEngine
+  }
+}
---
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/data/HapiFhirResourceDataSource.kt
@@ -0,0 +1,41 @@
+package com.argusoft.who.emcare.data
+
+import com.argusoft.who.emcare.api.ServerFhirService
+import com.google.android.fhir.sync.DataSource
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.hl7.fhir.r4.model.Bundle
+import org.hl7.fhir.r4.model.OperationOutcome
+import org.hl7.fhir.r4.model.Resource
+
+/** Implementation of the [FhirDataSource] that communicates with hapi fhir. */
+class HapiFhirResourceDataSource(private val service: ServerFhirService) : DataSource {
+
+  override suspend fun loadData(path: String): Bundle {
+    return service.getResource(path)
+  }
+
+  override suspend fun insert(resourceType: String, resourceId: String, payload: String): Resource {
+    return service.insertResource(
+      resourceType,
+      resourceId,
+      payload.toRequestBody("application/fhir+json".toMediaType())
+    )
+  }
+
+  override suspend fun update(
+    resourceType: String,
+    resourceId: String,
+    payload: String
+  ): OperationOutcome {
+    return service.updateResource(
+      resourceType,
+      resourceId,
+      payload.toRequestBody("application/json-patch+json".toMediaType())
+    )
+  }
+
+  override suspend fun delete(resourceType: String, resourceId: String): OperationOutcome {
+    return service.deleteResource(resourceType, resourceId)
+  }
+}
