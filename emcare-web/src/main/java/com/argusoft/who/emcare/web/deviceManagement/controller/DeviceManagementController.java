42dbf73b16b9538fca304061bf1e45d244367ee7
dvanparia <dvanparia@argusoft.in>
2021-12-01 17:55:56 +0530

    Removed unused imports

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/deviceManagement/controller/DeviceManagementController.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/deviceManagement/controller/DeviceManagementController.java
@@ -1,15 +1,12 @@
 package com.argusoft.who.emcare.web.deviceManagement.controller;
 
-import com.argusoft.who.emcare.web.deviceManagement.dao.DeviceManagementRepository;
 import com.argusoft.who.emcare.web.deviceManagement.model.DeviceManagement;
 import com.argusoft.who.emcare.web.deviceManagement.service.DeviceManagementService;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +18,6 @@ public class DeviceManagementController {
     
     @Autowired
     private DeviceManagementService deviceManagementService;
-    
-    @Autowired
-    private DeviceManagementRepository deviceManagementRepository;
     
     @GetMapping("")
     public List<DeviceManagement> retrieveDevices() {
