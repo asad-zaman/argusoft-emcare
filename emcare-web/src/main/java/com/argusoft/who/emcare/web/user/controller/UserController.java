0f761a671a8a9e42db632ade561ada450bfbc396
jkalariya <jkalariya@argusoft.in>
2021-12-08 13:56:43 +0530

    Add some bug fixing comment unused code

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/user/controller/UserController.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/user/controller/UserController.java
@@ -4,14 +4,19 @@ import com.argusoft.who.emcare.web.config.KeyCloakConfig;
 import com.argusoft.who.emcare.web.secuirty.EmCareSecurityUser;
 import com.argusoft.who.emcare.web.user.dto.UserDto;
 import com.argusoft.who.emcare.web.user.service.UserService;
+import org.keycloak.admin.client.CreatedResponseUtil;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -48,10 +53,11 @@ public class UserController {
      * @return
      */
     @PostMapping("/add")
-    public ResponseEntity<Object> addUser(@RequestBody UserDto user) {
-        UsersResource usersResource = KeyCloakConfig.getInstance().realm(KeyCloakConfig.realm).users();
-        CredentialRepresentation credentialRepresentation = createPasswordCredentials(user.getPassword());
+    public ResponseEntity<Object> addUser(@RequestBody UserDto user, HttpServletRequest request) {
 
+        RealmResource realmResource = KeyCloakConfig.getInstanceByAuth(request).realm(KeyCloakConfig.realm);
+        UsersResource usersResource = KeyCloakConfig.getInstanceByAuth(request).realm(KeyCloakConfig.realm).users();
+        CredentialRepresentation credentialRepresentation = createPasswordCredentials(user.getPassword());
         UserRepresentation kcUser = new UserRepresentation();
         kcUser.setUsername(user.getEmail());
         kcUser.setCredentials(Collections.singletonList(credentialRepresentation));
@@ -59,9 +65,28 @@ public class UserController {
         kcUser.setLastName(user.getLastName());
         kcUser.setEmail(user.getEmail());
         kcUser.setEnabled(true);
-//        kcUser.setClientRoles();
         kcUser.setEmailVerified(false);
-        usersResource.create(kcUser);
+        javax.ws.rs.core.Response response = usersResource.create(kcUser);
+        String userId = CreatedResponseUtil.getCreatedId(response);
+
+        RoleRepresentation testerRealmRole = realmResource.roles()//
+                .get("user").toRepresentation();
+//
+//        // Assign realm role tester to user
+        UserResource userResource = usersResource.get(userId);
+        userResource.roles().realmLevel() //
+                .add(Arrays.asList(testerRealmRole));
+
+//        ClientRepresentation app1Client = realmResource.clients() //
+//                .findByClientId("login-app").get(0);
+////
+//        // Get client level role (requires view-clients role)
+//        RoleRepresentation userClientRole = realmResource.clients().get("login-app") //
+//                .roles().get("user").toRepresentation();
+////
+////        // Assign client level role to user
+//        userResource.roles() //
+//                .clientLevel("login-app").add(Arrays.asList(userClientRole));
         return ResponseEntity.ok("Success");
     }
 
