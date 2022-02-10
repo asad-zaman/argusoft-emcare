0b8f04d0b8c787e56aedc795a4c1558c7f3c631b
Parth Panchal <ppanchal@argusoft.in>
2022-02-10 11:33:08 +0530

    added compare patients method.

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/deduplication/service/impl/DeduplicationServiceImpl.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/deduplication/service/impl/DeduplicationServiceImpl.java
@@ -1,19 +1,37 @@
 package com.argusoft.who.emcare.web.deduplication.service.impl;
 
 import com.argusoft.who.emcare.web.deduplication.service.DeduplicationService;
+import java.util.List;
+import org.hl7.fhir.r4.model.Address;
+import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Patient;
 
-
-public class DeduplicationServiceImpl implements DeduplicationService{
+public class DeduplicationServiceImpl implements DeduplicationService {
 
     @Override
     public Boolean comparePatients(Patient p1, Patient p2) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        //Flags | Note: Age Flag yet to be implemented.
+        Boolean isDuplicateFlag = false;
+        Boolean sameNameFlag = p1.hasName() && p2.hasName() && p1.getNameFirstRep().equalsDeep(p2.getNameFirstRep());
+        Boolean withoutNameFlag = !p1.hasName() && !p2.hasName();
+        Boolean sameAddressFlag = p1.hasAddress() && p2.hasAddress() && p1.getAddressFirstRep().equalsDeep(p2.getAddressFirstRep());
+        Boolean sameContactFlag = p1.hasContact() && p2.hasContact() && p1.getContactFirstRep().equalsDeep(p2.getContactFirstRep());
+        Boolean sameBirthDateFlag = p1.hasBirthDate() && p2.hasBirthDate() && p1.getBirthDate().equals(p2.getBirthDate());
+
+        //Tests
+        if (withoutNameFlag) {
+            isDuplicateFlag = (sameAddressFlag && sameContactFlag && sameBirthDateFlag);
+        } else {
+            isDuplicateFlag = (sameNameFlag && sameBirthDateFlag && (sameAddressFlag || sameContactFlag)); //Add age logic here
+        }
+
+        return isDuplicateFlag;
     }
 
     @Override
     public Boolean checkPatientDuplicates(Patient p) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-    
+
 }
