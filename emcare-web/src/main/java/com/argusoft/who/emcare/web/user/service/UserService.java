1f99aed7bd1751de001924eed262f9bd0fdac95d
jay <jkalariya@argusoft.in>
2022-04-22 11:56:14 +0530

    chnage in get user api

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/user/service/UserService.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/user/service/UserService.java
@@ -46,7 +46,7 @@ public interface UserService {
 
     public UserRepresentation getUserById(String userId);
 
-    public UserListDto getUserDtoById(String userId);
+    public MultiLocationUserListDto getUserDtoById(String userId);
 
     public ResponseEntity<Object> getUserRolesById(String userId);
 
