2de970925c93b860b1078dfa7d55b7095edc98f9
jay <jkalariya@argusoft.in>
2022-07-18 19:04:28 +0530

    Add chart api

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/dashboard/service/impl/DashboardServiceImpl.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/dashboard/service/impl/DashboardServiceImpl.java
@@ -1,10 +1,18 @@
 package com.argusoft.who.emcare.web.dashboard.service.impl;
 
+import com.argusoft.who.emcare.web.dashboard.dto.ChartDto;
 import com.argusoft.who.emcare.web.dashboard.service.DashboardService;
+import com.argusoft.who.emcare.web.fhir.dto.FacilityDto;
+import com.argusoft.who.emcare.web.fhir.service.LocationResourceService;
 import com.argusoft.who.emcare.web.userLocationMapping.dao.UserLocationMappingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
@@ -12,8 +20,40 @@ public class DashboardServiceImpl implements DashboardService {
     @Autowired
     UserLocationMappingRepository userLocationMappingRepository;
 
+    @Autowired
+    LocationResourceService locationResourceService;
+
     @Override
     public ResponseEntity<Object> getDashboardData() {
         return ResponseEntity.ok().body(userLocationMappingRepository.getDashboardData());
+    }
+
+    @Override
+    public ResponseEntity<Object> getDashboardBarChartData() {
+        List<ChartDto> barData = userLocationMappingRepository.getDashboardBarChartData();
+        List<ChartDto> pieData = userLocationMappingRepository.getDashboardPieChartData();
+        Map<String, Object> listMap = new HashMap<>();
+        List<String> names = new ArrayList<>();
+        List<Long> counts = new ArrayList<>();
+        for (ChartDto chartDto : barData) {
+            FacilityDto facilityDto = locationResourceService.getFacilityDto(chartDto.getFacilityId());
+            names.add(facilityDto.getFacilityName());
+            counts.add(chartDto.getCount());
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("names", names);
+        map.put("counts", counts);
+
+        List<Map<String, Object>> pieD = new ArrayList<>();
+        for (ChartDto chartDto : pieData) {
+            FacilityDto facilityDto = locationResourceService.getFacilityDto(chartDto.getFacilityId());
+            Map<String, Object> pie = new HashMap<>();
+            pie.put("name", facilityDto.getFacilityName());
+            pie.put("count", chartDto.getCount());
+            pieD.add(pie);
+        }
+        listMap.put("barChart", map);
+        listMap.put("pieChart", pieD);
+        return ResponseEntity.ok().body(listMap);
     }
 }
