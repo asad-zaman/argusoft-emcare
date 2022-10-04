188d5e9db646d3d601d2b892d8c4f86c576e0d27
jay <jkalariya@argusoft.in>
2022-10-04 18:29:54 +0530

    request return type application json

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/config/WebMvcConfig.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/config/WebMvcConfig.java
@@ -2,13 +2,15 @@ package com.argusoft.who.emcare.web.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig  implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${root}")
     private String root;
@@ -24,5 +26,17 @@ public class WebMvcConfig  implements WebMvcConfigurer {
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/resources/*");
 
+    }
+
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+
+        configurer.favorPathExtension(true).
+                favorParameter(false).
+                ignoreAcceptHeader(true).
+                useJaf(false).
+                defaultContentType(MediaType.APPLICATION_JSON).
+                mediaType("json", MediaType.APPLICATION_JSON);
     }
 }
