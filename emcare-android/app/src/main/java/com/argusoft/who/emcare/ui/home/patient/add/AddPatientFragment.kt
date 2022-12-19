b0528ac4852431cab9401945cb44802388a53c93
Parth Panchal <ppanchal@argusoft.in>
2022-12-19 12:18:08 +0530

    - fixed navigation bug on going back from patientQuestionnaireFragment to addPatientFragment

--- emcare-android/app/src/main/java/com/argusoft/who/emcare/ui/home/patient/add/AddPatientFragment.kt
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/ui/home/patient/add/AddPatientFragment.kt
@@ -5,6 +5,7 @@ import androidx.activity.addCallback
 import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.argusoft.who.emcare.R
 import com.argusoft.who.emcare.databinding.FragmentAddPatientBinding
 import com.argusoft.who.emcare.ui.common.*
@@ -82,7 +83,8 @@ class AddPatientFragment : BaseFragment<FragmentAddPatientBinding>() {
                     activity?.alertDialog {
                         setMessage(R.string.msg_continue_consultation)
                         setPositiveButton(R.string.button_yes) { _, _ ->
-                            navigate(R.id.action_addPatientFragment_to_patientQuestionnaireFragment) {
+                            findNavController().popBackStack(R.id.addPatientFragment, true)
+                            navigate(R.id.action_homeFragment_to_patientQuestionnaireFragment) {
                                 putString(INTENT_EXTRA_QUESTIONNAIRE_ID, it.questionnaireId)
                                 putString(INTENT_EXTRA_STRUCTUREMAP_ID, it.structureMapId)
                                 putString(INTENT_EXTRA_QUESTIONNAIRE_HEADER, it.questionnaireId)
