d509e549acd1980cf8fc5b52deddba9271d426ce
jkalariya <argusadmin>
2023-02-28 14:07:43 +0530

    Add Staging url

--- emcare-ui/src/app/shared/module/structor/src/environments/environment.ts
+++ emcare-ui/src/app/shared/module/structor/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: 'https://emcare.argusoft.com',
+    apiUrl: 'http://65.0.190.33:8080',
 };
--- emcare-ui/src/environments/environment.ts
+++ emcare-ui/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://emcare.argusoft.com',
-  questionnaireBuilderUrl: 'https://emcare.argusoft.com/questionnaireBuilder',
+  apiUrl: 'http://65.0.190.33:8080',
+  questionnaireBuilderUrl: 'http://65.0.190.33:3000/questionnaireBuilder',
   testUrl: 'http://localhost:4200/login',
   testUsername: 'emcare@gmail.com',
   testPassword: 'argusadmin'
