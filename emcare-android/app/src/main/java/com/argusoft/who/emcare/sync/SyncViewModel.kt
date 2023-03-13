99a4d7721447f88ed5cc005f4a5af93d7c844108
Parth Panchal <parth6606@gmail.com>
2023-03-12 23:06:55 -0700

    updated build.gradle to add buildConfigs for debug, staging & production.

--- emcare-android/app/src/main/java/com/argusoft/who/emcare/sync/SyncViewModel.kt
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/sync/SyncViewModel.kt
@@ -51,12 +51,12 @@ class SyncViewModel @Inject constructor(
                 }, 1, TimeUnit.SECONDS)
                 if (syncJobStatus is SyncJobStatus.Finished || emCareResult is SyncResult.Success) {
                     _syncState.value = (syncJobStatus is SyncJobStatus.Finished)?.let { ApiResponse.Success(syncJobStatus) }
-                    _syncState.value = null
+                    _syncState.value = ApiResponse.Success(null)
                     preference.writeLastSyncTimestamp(OffsetDateTime.now().toLocalDateTime().format(
                         DateTimeFormatter.ofPattern(formatString12)))
                 } else if( syncJobStatus is SyncJobStatus.InProgress) {
                     _syncState.value = ApiResponse.Success(syncJobStatus)
-                    _syncState.value = null
+                    _syncState.value = ApiResponse.Success(null)
 
                 } else {
                     _syncState.value = (syncJobStatus is SyncJobStatus.Failed)?.let { ApiResponse.ApiError(apiErrorMessageResId = R.string.msg_sync_failed) }
