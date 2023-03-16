763a910c9eee6bfc903d7e9ad73b5ca7ba8a2da8
Vishal <hsoni@argusoft.in>
2023-03-16 12:17:25 +0530

    minor fix

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/questionnaireresponse/dto/MiniPatient.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/questionnaireresponse/dto/MiniPatient.java
@@ -1,6 +1,10 @@
 package com.argusoft.who.emcare.web.questionnaireresponse.dto;
 
+import java.util.Date;
+
 public interface MiniPatient {
 
     public String getPatientId();
+
+    public Date getConsultationDate();
 }
