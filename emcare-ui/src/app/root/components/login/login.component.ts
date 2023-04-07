236ce9410591a80a503974bf5ad899aaad1518f0
Vishal <hsoni@argusoft.in>
2023-04-07 10:33:09 +0530

    fixing issue for maximum call size excedded

--- emcare-ui/src/app/root/components/login/login.component.ts
+++ emcare-ui/src/app/root/components/login/login.component.ts
@@ -120,10 +120,11 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('Username', res.userName);
         const isSuperAdmin = res['roles'].findIndex(el => el === 'SUPER_ADMIN') > -1;
         localStorage.setItem('isSuperAdmin', `${isSuperAdmin}`);
-        this.authService.setFeatures(res['feature']);
+        this.authService.setIsLoggedIn(true);
+        if (!isSuperAdmin)
+          this.authService.setFeatures(res['feature']);
         isSuperAdmin ? this.router.navigate(["/tenantList"]) : this.router.navigate(["/dashboard"]);
         this.toasterService.showToast('success', 'Welcome to EmCare!', 'EMCARE');
-        this.authService.setIsLoggedIn(true);
       }
     });
   }
