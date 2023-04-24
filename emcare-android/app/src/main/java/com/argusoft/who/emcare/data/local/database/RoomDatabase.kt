f8a5fe330cd4af3fce2fe37f7c63c4598e54ec12
Parth Panchal <parth6606@gmail.com>
2023-04-23 23:57:50 -0700

    - integrated country name to the title.

--- emcare-android/app/src/main/java/com/argusoft/who/emcare/data/local/database/RoomDatabase.kt
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/data/local/database/RoomDatabase.kt
@@ -6,11 +6,11 @@ import androidx.room.migration.AutoMigrationSpec
 import com.argusoft.who.emcare.ui.common.model.*
 
 @Database(entities = [Language::class, LoggedInUser::class, Facility::class, ConsultationFlowItem::class],
-    version = 1,
-//    autoMigrations = [
-//        AutoMigration (from = 1, to = 2,
-//            spec = RoomDatabase.RoomAutoMigrationVersion1To2::class)
-//    ],
+    version = 2,
+    autoMigrations = [
+        AutoMigration (from = 1, to = 2,
+            spec = RoomDatabase.RoomAutoMigrationVersion1To2::class)
+    ],
     exportSchema = true)
 @TypeConverters(Converters::class)
 abstract class RoomDatabase : androidx.room.RoomDatabase() {
--- emcare-android/app/src/main/java/com/argusoft/who/emcare/ui/home/HomeFragment.kt
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/ui/home/HomeFragment.kt
@@ -24,6 +24,7 @@ import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 import kotlin.math.roundToInt
 
 @AndroidEntryPoint
@@ -43,7 +44,7 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(){
     }
 
     override fun initView() {
-        binding.headerLayout.toolbar.setTitleDashboard(id = getString(R.string.title_home))
+        binding.headerLayout.toolbar.setTitleDashboard(id = getString(R.string.title_home) + (if(preference.getLoggedInUser() != null) " " + Locale("",preference.getLoggedInUser()?.countryCode).displayCountry else ""))
         homePagerAdapter = HomePagerAdapter(this, PatientListFragment(), ConsultationListFragment())
         binding.viewPager2.adapter = homePagerAdapter
         binding.viewPager2.isUserInputEnabled = false
