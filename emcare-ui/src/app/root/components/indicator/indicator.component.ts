d3a6e862ed4589a7aafddd47a63da22f170cbed1
Vishal <hsoni@argusoft.in>
2023-04-28 11:11:00 +0530

    temp commit

--- emcare-ui/src/app/root/components/indicator/indicator.component.ts
+++ emcare-ui/src/app/root/components/indicator/indicator.component.ts
@@ -60,6 +60,7 @@ export class IndicatorComponent implements OnInit {
   denEqCOnditionArr = [];
   numeratorEquationStringArr = [];
   denominatorEquationStringArr = [];
+  color = '#fff';
 
   constructor(
     private readonly formBuilder: FormBuilder,
