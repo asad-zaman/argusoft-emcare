e3106d7c990ea566a8a5565a44a1808227a31e6f
Parth Panchal <parth6606@gmail.com>
2023-05-31 21:08:05 -0700

    - refactored about fragment, viewmodel - made a common method for menuItemSyncAndMore listener

--- emcare-android/app/src/main/java/com/argusoft/who/emcare/ui/home/about/AboutViewModel.kt
+++ emcare-android/app/src/main/java/com/argusoft/who/emcare/ui/home/about/AboutViewModel.kt
@@ -5,22 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.argusoft.who.emcare.data.remote.ApiResponse
-import com.argusoft.who.emcare.ui.common.DATE_FORMAT
-import com.argusoft.who.emcare.ui.common.model.ActiveConsultationData
-import com.argusoft.who.emcare.ui.common.model.PreviousConsultationData
-import com.argusoft.who.emcare.ui.common.stageToBadgeMap
-import com.argusoft.who.emcare.ui.common.stageToIconMap
-import com.argusoft.who.emcare.ui.home.ConsultationFlowRepository
-import com.argusoft.who.emcare.ui.home.patient.PatientRepository
-import com.argusoft.who.emcare.utils.listener.SingleLiveEvent
 import com.google.android.fhir.FhirEngine
 import com.google.android.fhir.search.Order
 import com.google.android.fhir.search.search
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.hl7.fhir.r4.model.PlanDefinition
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
@@ -32,7 +22,7 @@ class AboutViewModel @Inject constructor(
     private val _bundleVersion = MutableLiveData<ApiResponse<String?>>()
     val bundleVersion: LiveData<ApiResponse<String?>> = _bundleVersion
 
-
+    //Stores the version of latest planDefinition as the IG Bundle Version.
     fun getBundleVersionNumber() {
         viewModelScope.launch {
             val planDefinitions = fhirEngine.search<PlanDefinition> {
