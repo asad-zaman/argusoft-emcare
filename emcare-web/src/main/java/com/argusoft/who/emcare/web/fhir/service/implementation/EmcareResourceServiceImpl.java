90d23661470628c341950ade1ae45d2cac08d553
jay <jkalariya@argusoft.in>
2023-06-07 16:15:45 +0530

    add log

--- emcare-web/src/main/java/com/argusoft/who/emcare/web/fhir/service/implementation/EmcareResourceServiceImpl.java
+++ emcare-web/src/main/java/com/argusoft/who/emcare/web/fhir/service/implementation/EmcareResourceServiceImpl.java
@@ -149,6 +149,7 @@ public class EmcareResourceServiceImpl implements EmcareResourceService {
         System.out.println("Resource Id ================"+ resourceId);
 
         String resourceString = parser.encodeResourceToString(resource);
+        System.out.println(resourceString);
         switch (resourceType.toUpperCase()) {
             case CommonConstant.FHIR_PATIENT:
                 EmcareResource emcareResource = findByResourceId(resourceId);
